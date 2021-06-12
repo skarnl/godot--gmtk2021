767a8380562f20266a93dad6d5236861aa8744dc
Oskar van Velden <oskar@rakso.nl>
2021-06-12 13:39:42 +0200

    Add helper utilities

---
+++ utils/helpers.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+const VARIANTS = 4
+
+
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+
+
+func _init() -> void:
+	rng.randomize()
+
+
+## Get random frame
+## (optional) excludes {array} with numbers to ignore
+func get_random_frame(excludes = []) -> int:
+	var result = rng.randi_range(0, VARIANTS - 1)
+	
+	while excludes.has(result):
+		result = rng.randi_range(0, VARIANTS - 1)
+		
+	return result
+
+
+func clone(config: Configuration) -> Configuration:
+	return Configuration.new(config._to_dictionary())
