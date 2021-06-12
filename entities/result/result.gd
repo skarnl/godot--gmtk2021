43e435bcd3e2f37acaa558bc684979a485969b80
Oskar van Velden <oskar@rakso.nl>
2021-06-12 16:20:25 +0200

    Add debug draw to show if the result fits the target

--- entities/result/result.gd
+++ entities/result/result.gd
@@ -1,5 +1,13 @@
 extends "res://entities/person/person.gd"
 
+var mark_as_success := false setget _set_mark_as_success
 
-func _ready() -> void:
-	pass
+func _draw() -> void:
+	if mark_as_success:
+		draw_rect(Rect2(Vector2(10.0, 10.0), Vector2(190.00, 190.00)), Color.green.lightened(0.2))
+
+
+func _set_mark_as_success(new_value):
+	mark_as_success = new_value
+	
+	update()
