76f3228b1d03ac572d1a5be8b9293949988ac35d
Oskar van Velden <oskar@rakso.nl>
2021-06-13 12:38:20 +0200

    Setup main menu

--- screens/main_menu.gd
+++ screens/main_menu.gd
@@ -4,12 +4,12 @@ signal start_game
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Button.grab_focus()
+	$StartButton.grab_focus()
 	show()
 
 
-
-func _on_Button_pressed():
+func _on_StartButton_pressed() -> void:
 	Game.start_game()
 	
 	hide()
+
