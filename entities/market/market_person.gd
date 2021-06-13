fbf648e02b28144c58aaf144a43e8ae8420517e9
Oskar van Velden <oskar@rakso.nl>
2021-06-13 09:14:56 +0200

    Set mouse cursor to pointing hand

--- entities/market/market_person.gd
+++ entities/market/market_person.gd
@@ -66,3 +66,10 @@ func get_configuration() -> Configuration:
 
 func is_selected() -> bool:
 	return _selected
+
+
+func _on_mouse_entered() -> void:
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+
+func _on_mouse_exited() -> void:
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
